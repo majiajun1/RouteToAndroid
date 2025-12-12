5a05fce60a0b7e324ccb630ed2b8a3780a2f00c2
majiajun <masonjj@foxmail.com>
2025-12-12 15:51:56 +0800

    2025121215551

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.databinding.ActivityMain2Binding;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.ActivityThirdBinding;
+import com.example.myapplication.databinding.TitleBinding;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    private ActivityThirdBinding activityBinding;
+    private TitleBinding titleBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+
+        getSupportActionBar().hide();
+        activityBinding=ActivityThirdBinding.inflate(getLayoutInflater());
+        setContentView(activityBinding.getRoot());
+        titleBinding=activityBinding.topTitle;
+        titleBinding.titleEdit.setOnClickListener(
+                v ->{
+                    Toast.makeText(this,"test button",LENGTH_SHORT).show();
+                }
+        );
+
     }
 }
